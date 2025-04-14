135a937d075667fc7212cbb3c6e621324aff646d
mekundur <you@example.com>
2025-04-14 11:50:03 +0200

    ft_popen working solution

---
+++ ft_popen/ft_popen.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+// solution that worked in the exam!
+
+int	ft_popen(char const *file, char *const argv[], char type)
+{
+	int	fd[2];
+	pipe(fd);
+	if (type == 'r')
+	{
+		if (!fork())
+		{
+			dup2(fd[1], 1);
+			close(fd[1]);
+			close(fd[0]);
+			execvp(file, argv);	
+			exit(1);	
+		}
+		close(fd[1]);
+		return(fd[0]);
+	}	
+	else if ((type) == 'w')
+	{
+		if(!fork())
+		{
+			dup2(fd[0], 0);
+			close(fd[0]);
+			close(fd[1]);
+			execvp(file, argv);	
+			exit(1);	
+		}
+		close(fd[0]);
+		return(fd[0]);	
+	}
+	else
+		return (1);
+}
+
+int	main(void)
+{
+	char	*line;
+	FILE	*file;
+
+//	file = popen("ls", "r");
+//	getline(&line, NULL, file);
+	
+	int	fd = ft_popen("ls", (char *const[]){"ls", "-l", NULL}, 'r');
+	printf("fd: %d\n", fd);
+	line = get_next_line(fd);
+	
+	puts(line);
+
+		
+//	int	fd = ft_popen("who", (char *const[]){"who", NULL}, 'w');
+
+
+
+}
+
