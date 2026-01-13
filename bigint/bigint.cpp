badfc4c6ecbe057c388db0f3790e6ffb053c897d
mekundur <mertkunn@gmail.com>
2026-01-13 01:43:19 +0100

    bigint solution

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -1,1 +1,11 @@
 #include "bigint.hpp"
+
+std::ostream& operator<<(std::ostream& out, const bigint& bigint) {
+	
+	std::vector<int>::const_reverse_iterator it = bigint.get_digits().rbegin();
+	
+	for (; it != bigint.get_digits().rend(); ++it)
+		out << *it;
+	
+	return out;
+}
