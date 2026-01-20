640ef4a342dae25f325d1b350e94edb6b9951705
mekundur <mertkunn@gmail.com>
2026-01-20 23:33:39 +0100

    cleanup

--- bigint/bigint.cpp
+++
@@ -1,8 +0,0 @@
-#include "bigint.hpp"
-
-std::ostream& operator<<(std::ostream& out, const bigint& bigint) {
-	std::vector::const_reverse_iterator it = bigint.digits.rbegin();
-	for(; it != digits.rend(); it++)
-		out << bigint.digits;
-	return (out);
-}
